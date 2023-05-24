306a139e16a6f91cb8fb72e0625234ca5d626c4e
youjin kim <gracecancho@gmail.com>
2023-05-24 10:27:22 +0900

    try class component

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class Header extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <div>Logo</div>
+                    <input />
+                </div>
+            </>
+        )
+    }
+
+}
+
+export default Header;
